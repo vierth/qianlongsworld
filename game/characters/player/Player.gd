21b0d9fddec553930b65dea79ed7cb2d9de2e3ac
vierth <vierth@gmail.com>
2024-03-05 18:03:19 -0500

    add speed adjustment for player

--- game/characters/player/Player.gd
+++ game/characters/player/Player.gd
@@ -6,7 +6,7 @@ extends CharacterBody3D
 @onready var neck = $Neck
 @onready var cam = $Neck/Camera3D
 
-const SPEED = 5.0
+var speed = 5.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -24,6 +24,12 @@ func _unhandled_input(event):
 			neck.rotate_y(-event.relative.x * 0.01)
 			cam.rotate_x(-event.relative.y * 0.01)
 			cam.rotation.x = clamp(cam.rotation.x, deg_to_rad(-60), deg_to_rad(90))
+	if event.is_action_pressed("speed_up"):
+		speed += 1
+	if event.is_action_pressed("speed_down"):
+		speed -= 1
+		if speed < 0:
+			speed = 0
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -39,11 +45,11 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
 	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	if global_position.y < min_y:
 		global_position = warp_location
