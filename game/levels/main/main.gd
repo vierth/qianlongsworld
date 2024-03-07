302a7881b81c4e8eb946eedbf32f63fb42be6a87
vierth <vierth@gmail.com>
2024-03-07 10:27:50 -0500

    updates to main scene

---
+++ game/levels/main/main.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	#var new_scene =  load("res://levels/pvierthaler/46_3.tscn").instantiate()
+	#
+	#get_tree().root.add_child.call_deferred(new_scene)
+#
+	#var mesh_size = new_scene.get_child(0).get_child(0).get_aabb()
+	#print(new_scene.global_position)
+	#
+	#var new_pos = Vector3(new_scene.global_position.x + mesh_size.size.x/2,  new_scene.global_position.y, new_scene.global_position.z + mesh_size.size.z/2)
+	#new_scene.set_global_position(new_pos)
+	#print(mesh_size.size.x, mesh_size.position)
+	#new_scene.global_position.x = mesh_size.s
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
