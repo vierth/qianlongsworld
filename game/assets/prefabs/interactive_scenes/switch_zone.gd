868f15f1f90ab26fa94d3444bb026b0c7ff7a3a2
vierth <vierth@gmail.com>
2024-03-18 16:10:12 -0400

    Clear up warnings

--- game/assets/prefabs/interactive_scenes/switch_zone.gd
+++ game/assets/prefabs/interactive_scenes/switch_zone.gd
@@ -5,9 +5,5 @@ extends Area3D
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
-func _on_body_entered(body):
+func _on_body_entered(_body):
 	SceneSwitcher.switch_scene(destination_scene)
