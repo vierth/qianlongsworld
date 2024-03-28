11f3818af77a160d9b15cd9b62d6e69dea99cb31
vierth <vierth@gmail.com>
2024-03-28 11:49:42 -0400

    create title screen

---
+++ game/levels/GUI/titlescreen/title_screen.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_start_button_button_down():
+	SceneSwitcher.switch_scene("res://levels/main/main.tscn")
+
+func _on_quit_button_button_down():
+	get_tree().quit()
+
+
+func _on_settings_button_button_down():
+	pass # Replace with function body.
+
+
+func _on_credits_button_button_down():
+	pass # Replace with function body.
