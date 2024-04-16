55ce525adbf9ff0e0f27a2c8614af56e01ac598e
vierth <vierth@gmail.com>
2024-04-16 09:41:58 -0400

    Add dialogue system

---
+++ game/characters/npcs/emperors/qianlongemperor.gd
@@ -0,0 +1,31 @@
+extends CharacterBody3D
+# adapted from https://www.youtube.com/watch?v=Tmy1tzhDLl4
+
+var player_detected = false
+
+func _ready():
+	Dialogic.signal_event.connect(DialogicSignal)
+
+func _process(_delta):
+	if player_detected:
+		if Input.is_action_just_pressed("interact"):
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			player_detected.talking = true
+			run_dialogue("qianlongIntro")
+
+func _on_detection_area_body_entered(body):
+	if body.has_method("player"):
+	
+		player_detected = body
+
+func _on_detection_area_body_exited(body):
+	if body.has_method("player"):
+		player_detected = false # Replace with function body.
+
+func run_dialogue(dialogue_string):
+	Dialogic.start(dialogue_string)
+	
+func DialogicSignal(arg):
+	if arg == "exit":
+		player_detected.talking = false
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
