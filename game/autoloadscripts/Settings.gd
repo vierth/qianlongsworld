51a6df70e1eafa036b3c53a7b1adc4c8d70f372c
dhbergerwm <145725459+dhbergerwm@users.noreply.github.com>
2024-05-12 21:25:06 -0400

    Add font asset, Settings.gd

---
+++ game/autoloadscripts/Settings.gd
@@ -0,0 +1,26 @@
+extends Node
+
+var player_speed : int = 5
+
+var mouse_sensitivity : float = 0.01
+
+var current_scene : String
+
+var pos_x : float
+
+var pos_y : float
+
+var facing_x : float
+
+var facing_y : float
+
+var velocity_y : float
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
